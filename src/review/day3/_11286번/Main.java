8649c8778ae345405d58b031667eb31fecb58c71
woals8503 <woals6391@gmail.com>
2023-04-13 05:55:13 +0900

    day3 review complete

--- src/review/day3/_11286번/Main.java
+++ src/review/day3/_11286번/Main.java
@@ -1,36 +1,24 @@
-package basic.day3._11286번;
+package review.day3._11286번;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Comparator;
 import java.util.PriorityQueue;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        
-//        PriorityQueue<Integer> queue = new PriorityQueue<>(new Comparator<Integer>() {
-//            @Override
-//            public int compare(Integer o1, Integer o2) {
-//                //절대값으로 변환
-//                int first_abs = Math.abs(o1);   //내가 사용한 값
-//                int second_abs = Math.abs(o2);  //비교 대상
-//                if (first_abs == second_abs)    //동일한 값이면 순수 값 비교
-//                    return o1 > o2? 1 : -1;    // 1(true) 서로 위치 바꿈 -> 내림차순   -1 (false) 그대로 냅둠 -> 오름차순(default)
-//                else
-//                    return first_abs - second_abs;
-//            }
-//        });
 
         PriorityQueue<Integer> queue = new PriorityQueue<>((o1, o2) -> {
             int first_abs = Math.abs(o1);
             int second_abs = Math.abs(o2);
 
-            if(first_abs == second_abs) {
+            //절대값이 같을 경우
+            if(first_abs == second_abs)
                 return o1 > o2 ? 1 : -1;
-            }
+            //절대값이 다를 경우 내 자신 - 비교값  =>  음수일 경우 내자신이 더 작음
+            //                                =>  양수일 경우 내 자신이 더 큼
             else
                 return first_abs - second_abs;
         });
---
+++ src/review/day3/_2164번/Main.java
@@ -0,0 +1,25 @@
+package review.day3._2164번;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        Queue<Integer> q = new LinkedList<>();
+        for(int i=1; i<=N; i++)
+            q.add(i);
+
+        while(q.size() != 1) {
+            q.poll();
+            q.add(q.poll());
+        }
+
+        System.out.println(q.poll());
+    }
+}
